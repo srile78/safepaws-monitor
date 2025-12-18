823e446e70e169ec4057204b706ae9e548a9a0bf
srile78 <146881680+srile78@users.noreply.github.com>
2025-12-18 08:00:15 -0800

    chore: move adsense script to head for verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,13 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={nunito.className}>
-        <Script
-          id="adsbygoogle-init"
-          strategy="afterInteractive"
-          crossOrigin="anonymous"
+      <head>
+        <script
+          async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8082302563728806"
-        />
+          crossOrigin="anonymous"
+        ></script>
+      </head>
+      <body className={nunito.className}>
         {children}
 
         {/* Professional Footer */}
